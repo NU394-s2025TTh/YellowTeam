8c89c7e985a65537217e2900936a339a4555c1bc
Connor Lai <connorlai@hotmail.com>
2025-04-09 16:51:24 -0500

    Added firebase utils

--- src/firebase/utils.ts
+++ src/firebase/utils.ts
@@ -1,9 +1,17 @@
 import 'firebase/database';
 
-import { getDatabase, ref, set } from 'firebase/database';
+import { getDatabase, push, ref, set, update } from 'firebase/database';
 
 const setData = async (path: string, data: unknown) => {
   await set(ref(getDatabase(), path), data);
 };
 
-export { setData };
+const updateData = async (path: string, data: object) => {
+  await update(ref(getDatabase(), path), data);
+};
+
+const pushData = async (path: string, data?: unknown) => {
+  await push(ref(getDatabase(), path), data);
+};
+
+export { pushData, setData, updateData };
