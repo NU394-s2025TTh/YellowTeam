efca064039e10ccb582edbdb5f9dbb7c4a6ea7ba
Connor Lai <connorlai@hotmail.com>
2025-04-09 17:43:54 -0500

    Updated formatting of firebase integration

---
+++ src/types/ResortData.ts
@@ -0,0 +1,12 @@
+export type ResortData = {
+  name: string;
+  temperature: string;
+  snowfall: string;
+  precipitation: string;
+  wind: string;
+  openLifts: number;
+  totalLifts: number;
+  trailsOpen: number;
+  totalTrails: number;
+  checklist: string[];
+};
