fda3e87f441357e672732ed42b09006a133951cf
Connor Lai <connorlai@hotmail.com>
2025-04-09 17:01:59 -0500

    Updated format of Firebase configuration

--- src/firebase/utils.ts
+++ src/firebase/utils.ts
@@ -1,6 +1,19 @@
 import 'firebase/database';
 
+import { initializeApp } from 'firebase/app';
 import { getDatabase, push, ref, set, update } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB33D08gA80Fp7ZoW6d-BvS3wpasaGC1HM',
+  authDomain: 'powderprep-dev.firebaseapp.com',
+  databaseURL: 'https://powderprep-dev-default-rtdb.firebaseio.com',
+  projectId: 'powderprep-dev',
+  storageBucket: 'powderprep-dev.firebasestorage.app',
+  messagingSenderId: '266068817759',
+  appId: '1:266068817759:web:8225bd63e9e121dff568ea',
+};
+
+initializeApp(firebaseConfig);
 
 const setData = async (path: string, data: unknown) => {
   await set(ref(getDatabase(), path), data);
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,9 @@
 import './index.css';
 
-import { initializeApp } from 'firebase/app';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyB33D08gA80Fp7ZoW6d-BvS3wpasaGC1HM',
-  authDomain: 'powderprep-dev.firebaseapp.com',
-  databaseURL: 'https://powderprep-dev-default-rtdb.firebaseio.com',
-  projectId: 'powderprep-dev',
-  storageBucket: 'powderprep-dev.firebasestorage.app',
-  messagingSenderId: '266068817759',
-  appId: '1:266068817759:web:8225bd63e9e121dff568ea',
-};
-initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
